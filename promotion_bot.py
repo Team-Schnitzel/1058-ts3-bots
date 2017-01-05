ac281833d684b6790c867a9d28263908383c61e0
looney1058 <koenvds@live.nl>
2017-01-05 19:27:00 +0100

    added checking interval variable
    added checking interval variable and cleaned it up a bit.

--- promotion_bot.py
+++ promotion_bot.py
@@ -1,7 +1,8 @@
 import time
 import ts3
 import logging.config
-
+import logging
+import logging.handlers
 
 #General settings:
 BOT_NAME = ''
@@ -9,38 +10,18 @@ USER = ''
 PASS = ''
 HOST = ''
 PORT = ''
-SID = ''
+SID = ''                    #virtual server ID, usually 1
 
 #Promotion bot settings:
-CONNECTIONS_REQUIRED = 100
-DAYS_REQUIRED = 60
+CONNECTIONS_REQUIRED = 10   #the amount of total reconnect required
+DAYS_REQUIRED = 30          #the amount of required days between first and current connect
 PROMOTION_MESSAGE = 'You have been automatically promoted'
-PROMOTE_FROM = ''  #server group number
-PROMOTE_TO = ''    #server group number
-
-#Logging:
-import logging
-import logging.handlers
-f = logging.Formatter(fmt='%(levelname)s:%(name)s: %(message)s '
-    '(%(asctime)s; %(filename)s:%(lineno)d)',
-    datefmt="%Y-%m-%d %H:%M:%S")
-handlers = [
-    logging.handlers.RotatingFileHandler('promotion_bot.log', encoding='utf8',
-        maxBytes=100000, backupCount=5),
-    logging.StreamHandler()
-]
-root_logger = logging.getLogger()
-root_logger.setLevel(logging.DEBUG)
-for h in handlers:
-    h.setFormatter(f)
-    h.setLevel(logging.DEBUG)
-    root_logger.addHandler(h)
-
-
+PROMOTE_FROM = '8'          #server group number (8 is guest by default)
+PROMOTE_TO = '7'            #server group number (7 is member by default)
+CHECKING_INTERVAL = 10      #checking interval in seconds
 
 def ts3_bot(server):
-
-        #Promotion bot (prom_b)
+    while True:
         # Process to promote clients
         for client in server.clientlist():
             if client["client_type"] != '1' :
@@ -61,12 +42,32 @@ def ts3_bot(server):
                         server.sendtextmessage(targetmode=1, target=client['clid'], msg=PROMOTION_MESSAGE)
                 except ts3.query.TS3QueryError:
                     pass
-        time.sleep(1)
+        time.sleep(CHECKING_INTERVAL)
 
 
 if __name__ == "__main__":
+    # Connect to server
     with ts3.query.TS3Connection(HOST, PORT) as ts3conn:
         ts3conn.login(client_login_name=USER, client_login_password=PASS)
         ts3conn.use(sid=SID)
         ts3conn.clientupdate(client_nickname=BOT_NAME)
+
+        # Logging
+        f = logging.Formatter(fmt='%(levelname)s:%(name)s: %(message)s '
+                                  '(%(asctime)s; %(filename)s:%(lineno)d)',
+                              datefmt="%Y-%m-%d %H:%M:%S")
+        handlers = [
+            logging.handlers.RotatingFileHandler('promotion_bot.log', encoding='utf8',
+                                                 maxBytes=100000, backupCount=5),
+            logging.StreamHandler()
+        ]
+        root_logger = logging.getLogger()
+        root_logger.setLevel(logging.DEBUG)
+        for h in handlers:
+            h.setFormatter(f)
+            h.setLevel(logging.DEBUG)
+            root_logger.addHandler(h)
+        logging.info('1058 Promotion bot started')
+
+        # Run bot
         ts3_bot(ts3conn)
